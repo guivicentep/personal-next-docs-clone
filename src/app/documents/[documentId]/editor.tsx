8972d243e072069831f5f42c8546dd27a11f1aa2
guivicentep <guivicente.gvp@hotmail.com>
2025-01-02 10:50:13 -0300

    feat: Margin ruler component

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -21,11 +21,13 @@ import { Color } from '@tiptap/extension-color'
 import Highlight from '@tiptap/extension-highlight'
 import Link from '@tiptap/extension-link'
 import TextAlign from '@tiptap/extension-text-align'
+import { Ruler } from './ruler'
 
 export const Editor = () => {
   const { setEditor } = useEditorStore()
 
   const editor = useEditor({
+    immediatelyRender: false,
     onCreate({ editor }) {
       setEditor(editor)
     },
@@ -110,11 +112,10 @@ export const Editor = () => {
 
   return (
     <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
+      <Ruler />
       <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
         <EditorContent editor={editor} />
       </div>
     </div>
   )
 }
-
-// 3:05:13
