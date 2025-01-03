64e6c91f46d635d93b870a26106f5f1f2e3bcf67
guivicentep <guivicente.gvp@hotmail.com>
2025-01-03 16:15:05 -0300

    feat: Authentication setup with Clerk

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  providers: [
+    {
+      domain: 'https://legal-firefly-37.clerk.accounts.dev',
+      applicationID: 'convex',
+    },
+  ],
+}
