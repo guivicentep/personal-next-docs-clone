f5b6f110a0a49c6988a43bcdb724f53e0a96d8cd
guivicentep <guivicente.gvp@hotmail.com>
2025-01-12 22:56:39 -0300

    feat: Document information

---
+++ src/hooks/use-debounce.ts
@@ -0,0 +1,20 @@
+import { useCallback, useRef } from 'react'
+
+export function useDebounce<
+  T extends (...args: Parameters<T>) => ReturnType<T>,
+>(callBack: T, delay: number = 500) {
+  const timeoutRef = useRef<NodeJS.Timeout>()
+
+  return useCallback(
+    (...args: Parameters<T>) => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current)
+      }
+
+      timeoutRef.current = setTimeout(() => {
+        callBack(...args)
+      }, delay)
+    },
+    [callBack, delay],
+  )
+}
